70ca2bb93835999786f6b6676a4ca474b789a1ff
jshemas <joshshemas@gmail.com>
2024-07-12 00:00:56 -0400

    adding options.html back into the docs

--- index.ts
+++ index.ts
@@ -12,6 +12,7 @@ import type {
  * @param {object} options - The options used by Open Graph Scraper
  * @param {boolean} [options.onlyGetOpenGraphInfo] - Only fetch open graph info and don't fall back on anything else.
  * @param {object} [options.customMetaTags] - Here you can define custom meta tags you want to scrape.
+ * @param {string} options.html - You can pass in an HTML string to run ogs on it. (use without options.url)
  * @returns {Promise} Promise Object with the Open Graph results
  */
 export default async function run(options: OpenGraphScraperOptions): Promise<ErrorResult | SuccessResult> {
