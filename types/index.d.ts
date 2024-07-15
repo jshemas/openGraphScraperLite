6fa01d1ce4d92cacaf2c076d3aa6eaaae8b5c132
jshemas <joshshemas@gmail.com>
2024-07-14 21:27:45 -0400

    adding types to project and adding better support for both cjs and esm

---
+++ types/index.d.ts
@@ -0,0 +1,25 @@
+import type { OpenGraphScraperOptions, OgObject } from './lib/types';
+/**
+ * `open-graph-scraper` uses [fetch](https://developer.mozilla.org/en-US/docs/Web/API/fetch) for http requests
+ * for scraping Open Graph and Twitter Card info off a website.
+ *
+ * @param {object} options - The options used by Open Graph Scraper
+ * @param {boolean} [options.onlyGetOpenGraphInfo] - Only fetch open graph info and don't fall back on anything else.
+ * @param {object} [options.customMetaTags] - Here you can define custom meta tags you want to scrape.
+ * @param {string} options.html - You can pass in an HTML string to run ogs on it. (use without options.url)
+ * @returns {Promise} Promise Object with the Open Graph results
+ */
+export default function run(options: OpenGraphScraperOptions): Promise<ErrorResult | SuccessResult>;
+export interface SuccessResult {
+    error: false;
+    html: string;
+    result: OgObject;
+}
+export interface ErrorResult {
+    error: true;
+    html: undefined;
+    result: OgObject;
+}
+
+// adding this line in to fix ESM imports for `module/moduleResolution = NodeNext` projects
+export = run;
---
+++ types/lib/openGraphScraper.d.ts
@@ -0,0 +1,15 @@
+import type { OpenGraphScraperOptions } from './types';
+/**
+ * sets up options for the fetch request and calls extract on html
+ *
+ * @param {object} options - options for ogs
+ * @return {object} object with ogs results
+ *
+ */
+export default function setOptionsAndReturnOpenGraphResults(ogsOptions: OpenGraphScraperOptions): Promise<{
+    ogObject: import("./types").OgObjectInteral;
+    response: {
+        body: string;
+    };
+    html: string;
+}>;
